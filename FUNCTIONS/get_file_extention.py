6c4f4d5a20e1d7c271c6640d0d7702c178e18986
DestinyNnamdi <diddyglitz1@outlook.com>
2024-12-31 12:29:44 +0100

    Edited readme file and dropped file extension assignment

---
+++ FUNCTIONS/get_file_extention.py
@@ -0,0 +1,17 @@
+# get the file extention of any file extention .
+# question: define a python function that:
+# is called "file_extention
+# takes the argument called file:
+# returns the file extention of the argument (file)
+# examples:
+# kefas.py should return .py, setup.exe should return .exe
+# nvda setup version 2024.1.1.exe should return .exe
+# destiny.serina.daniel.png should return .png
+# or even music.mp3, android.java
+# remember: the file extention can be more or less than 3 like .java, .c, .py, .nvda-addon, and so on.
+#good luck.
+# start your code below:
+def file_extension(destiny):
+    return(f".png")
+
+
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+
 # PythonicBasicClass
-I am learning python programming under the tutelage of Kefas Lungu.
+
+PythonicBasicClass is designed to equip students with foundation knowledge of python programming.
+
+***Lessons Learnt So Far Include;***
+*Variables and Data Types*
+*Python Operators*
+*Strings Manipulation and Methods*
+*Functions*
+*Git and Github*
+*Introduction To Looping*
+
+> My intention is to used the knowledge gained to develop software that would benefit humanity.
+>
+>> A solid foundation in python programming is critical to realising this goal.
+
+
+
+
